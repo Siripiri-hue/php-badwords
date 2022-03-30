4c71f781139127210ae58f1a44dce5a8f321061e
daemonprocellae@live.it <daemonprocellae@live.it>
2022-03-30 15:35:00 +0200

    Aggiunto html

--- index.php
+++ index.php
@@ -7,15 +7,35 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 // definisco il testo
 $text = "Io abito a New York, e pensavo al laghetto di Central Park, vicino a Central Park South. Chi sa se quando arrivavo a casa l’avrei trovato gelato, mi domandavo, e se era gelato, dove andavano le anitre? Chi sa dove andavano le anitre quando il laghetto era tutto gelato e col ghiaccio sopra. Chi sa se qualcuno andava a prenderle con un camion per portarle allo zoo o vattelapesca dove. O se volavano via.";
 
-echo $text; //stampo il testo
-echo "<br>"; //inserisco un br
-echo "La lunghezza del paragrafo è: " . strlen($text); //stampo la lunghezza del paragrafo
-echo "<br>"; //inserisco un br
+// echo $text; //stampo il testo
+// echo "<br>"; //inserisco un br
+// echo "La lunghezza del paragrafo è: " . strlen($text); //stampo la lunghezza del paragrafo
+// echo "<br>"; //inserisco un br
 
 $censured_word = $_GET['word']; //scelgo una parola da rimpiazzare
 $replaced_text = str_replace($censured_word, "***", $text); //rimpiazzo la parola selezionata con gli asterischi
 
-echo $replaced_text; //stampo il testo modificato
-echo "<br>"; //inserisco un br
-echo "La lunghezza del paragrafo con la parola rimpiazzata è: " . strlen($replaced_text); //stampo la lunghezza del testo modificato
-echo "<br>"; //inserisco un br
+// echo $replaced_text; //stampo il testo modificato
+// echo "<br>"; //inserisco un br
+// echo "La lunghezza del paragrafo con la parola rimpiazzata è: " . strlen($replaced_text); //stampo la lunghezza del testo modificato
+// echo "<br>"; //inserisco un br
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <h2>Testo originale</h2>
+    <p> <?php echo $text; ?> </p>
+    <p>La lunghezza del paragrafo è: <?php echo strlen($text); ?> </p>
+    <h2>Testo censurato</h2>
+    <p> <?php echo $replaced_text; ?> </p>
+    <p>La lunghezza del paragrafo con la parola rimpiazzata è: <?php echo strlen($replaced_text); ?> </p>
+</body>
+</html>
